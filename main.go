bebbd0351da08d19c1dff7be52324ca5e1f80194
Alex Wouda <awouda@gmail.com>
2019-01-30 21:18:40 +0100

    added create note

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"github.com/awouda/notes-api/domain"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"net/http"
 	"github.com/go-chi/render"
-	"github.com/awouda/notes-api/domain"
+	"math/rand"
+	"net/http"
+	"strings"
 )
 
 
 type AppNote domain.Note
 
+
+
 func main() {
+
+    //example commandline parsing, maybe we need it later
+	//var count = flag.Int("count", 5, "the count of items")
+	//flag.Parse()
+	//fmt.Println("count value ", *count)
+
+
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -20,26 +34,58 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Route("/notes", func(r chi.Router) {
-		r.Get("/", listNotes)
+		r.Get("/", ListNotes)
+		r.Post("/", CreateNote)
 	})
 
 	http.ListenAndServe(":3000", r)
 
 }
 
-func listNotes(w http.ResponseWriter, r *http.Request) {
-
-	render.Render(w, r, NewNoteResponse(notes[0]))
-	//if err := render.Render(w, r, Note{"a","aaa"}.Render(w,r)); err != nil {
-	//	render.Render(w, r, ErrRender(err))
-	//	return
-	//}
+func ListNotes(w http.ResponseWriter, r *http.Request) {
+	if err := render.RenderList(w, r, NewArticleListResponse(notes)); err != nil {
+		render.Render(w, r, ErrRender(err))
+		return
+	}
 }
+
+func CreateNote(w http.ResponseWriter, r *http.Request) {
+	data := &NoteRequest{}
+	if err := render.Bind(r, data); err != nil {
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+
+	article := data.AppNote
+	dbNewNote(article)
+
+	render.Status(r, http.StatusCreated)
+	render.Render(w, r, NewNoteResponse(article))
+}
+
 
 func NewNoteResponse(note *AppNote) *NoteResponse {
 	resp := &NoteResponse{AppNote: note}
 
 	return resp
+}
+
+
+type NoteListResponse []*NoteResponse
+
+func NewArticleListResponse(notes []*AppNote) []render.Renderer {
+	list := []render.Renderer{}
+	for _, note := range notes {
+		list = append(list, NewNoteResponse(note))
+	}
+	return list
+}
+
+func dbNewNote(note *AppNote) (string, error) {
+	note.ID = fmt.Sprintf("%d", rand.Intn(100)+10)
+	notes = append(notes, note)
+	fmt.Println(len(notes))
+	return note.ID, nil
 }
 
 type ErrResponse struct {
@@ -55,11 +101,39 @@ type ErrResponse struct {
 //	return nil
 //}
 
+
+
 type NoteResponse struct {
 	*AppNote
 	// We add an additional field to the response here.. such as this
 	// elapsed computed property
 	Elapsed int64 `json:"elapsed"`
+}
+type NoteRequest struct {
+	*AppNote
+	// We add an additional field to the response here.. such as this
+	// elapsed computed property
+	User string `json:"user,omitempty"`
+}
+
+func (n *NoteRequest) Bind(r *http.Request) error {
+	// a.Article is nil if no Article fields are sent in the request. Return an
+	// error to avoid a nil pointer dereference.
+	if n.AppNote == nil {
+		return errors.New("missing required Note fields.")
+	}
+
+
+	if n.User == "" {
+       fmt.Println("user was empty")
+	} else {
+		fmt.Println("user was ", n.User)
+	}
+
+
+
+	n.AppNote.Content = strings.ToLower(n.Content) // as an example, we down-case
+	return nil
 }
 
 func (rd *NoteResponse) Render(w http.ResponseWriter, r *http.Request) error {
@@ -67,6 +141,19 @@ func (rd *NoteResponse) Render(w http.ResponseWriter, r *http.Request) error {
 	rd.Elapsed = 10
 	return nil
 }
+
+
+
+
+func ErrInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
+}
+
 
 
 var notes = []*AppNote{
